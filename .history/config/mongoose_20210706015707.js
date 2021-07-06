df5ef2e5c891d717f6610a1e554b81b62c8778a6
Aditya <adi.agrawal267@gmail.com>
2021-07-06 13:47:12 +0530

    initial commit

---
+++ .history/config/mongoose_20210706015707.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const dd='mongodb+srv://aditya:<password>@cluster0.qbvk1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+mongoose.connect('mongodb://localhost/codeial_development');
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+
+
+db.once('open', function(){
+    console.log('Connected to Database :: MongoDB');
+});
+
+
+module.exports = db;
